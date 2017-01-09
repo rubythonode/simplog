2ca671fffd7c77557a9e0c7e48f525c5e59e431d
dhpark <dhpark0648@gmail.com>
2017-01-09 10:02:17 +0900

    d

--- assets/js/simplog.js
+++ assets/js/simplog.js
@@ -10,7 +10,7 @@ function sendMail(){
   var email = $('#user_email');
   var name = $('#user_name');
   var content = $('#user_content');
-  var subject = "Contact from " + name.val() ;
+  var subject = "contact from " + name.val() ;
 
   // email validation check
   var regExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i ;
